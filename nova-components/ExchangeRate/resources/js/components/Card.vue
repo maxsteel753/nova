92f4c967ef7a1fd7a479cfc809b41b937ff943c5
Abhishek Sharma <abhishek@Abhisheks-MacBook-Pro.local>
2021-01-18 15:10:24 +0530

    33

---
+++ nova-components/ExchangeRate/resources/js/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+    <card class="flex flex-col items-center justify-center">
+        <div class="px-12 py-12">
+            <h1 class="text-center text-3xl text-80 font-light">Exchange Rates(INR)</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>USD</th>
+                        <th>GBP</th>
+                        <th>EUR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ rate.USD }}</td>
+                        <td>{{ rate.GBP }}</td>
+                        <td>{{ rate.EUR }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+    </card>
+</template>
+
+<script>
+export default {
+    props: [
+        'card',
+
+        // The following props are only available on resource detail cards...
+        // 'resource',
+        // 'resourceId',
+        // 'resourceName',
+    ],
+    data : function() {
+        return {
+            rate: []
+        }
+    },
+    computed: async function() {
+        let r= this;
+        const response = await fetch('https://api.exchangeratesapi.io/latest?base=INR');
+        let data =  await response.json();    
+        r.rate.USD = data.rates.USD;
+        r.rate.GBP = data.rates.GBP;
+        r.rate.EUR = data.rates.EUR;
+          
+    
+        
+    }
+}
+</script>
---
+++ nova-components/ExchangeRate/src/ExchangeRate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyApp\ExchangeRate;
+
+use Laravel\Nova\Card;
+
+class ExchangeRate extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = '1/3';
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component()
+    {
+        return 'exchange-rate';
+    }
+}
